02829f6ab95d0baabea89abe70c122a2e96bc95f
ShawnHymel <5232145+ShawnHymel@users.noreply.github.com>
2026-01-21 20:10:43 -0700

    Cleaned up training scripts, added expected logits to test sample files.

--- projects/02_gesture_classification/model/standardization.h
+++ projects/02_gesture_classification/model/standardization.h
@@ -1,12 +1,22 @@
-// Sensor channels: accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z
+/* Sensor channels: accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z */
 #define NUM_CHANNELS 6
 
-// Mean for each sensor channel
+/* Mean for each sensor channel */
 const float STANDARDIZATION_MEANS[NUM_CHANNELS] = {
     -104.811367f, -367.461367f, 1497.516767f, -27.567033f, 26.882467f, -107.321133f
 };
 
-// Standard deviation for each sensor channel
+/* Standard deviation for each sensor channel */
 const float STANDARDIZATION_STD_DEVS[NUM_CHANNELS] = {
     715.407469f, 991.475474f, 1131.553970f, 1132.640657f, 1511.256275f, 1366.474267f
+};
+
+/* Labels */
+#define NUM_CLASSES 5
+const char* class_names[] = {
+    "_idle",
+    "_other",
+    "alpha",
+    "beta",
+    "gamma",
 };
